305d72d849d1fdb711622d1666a696c31e888e6b
Marshall Rose <mrose17@gmail.com>
2014-06-27 23:58:22 -0700

    add support for most common RRs

--- DNSKit/DNSResponse.h
+++ DNSKit/DNSResponse.h
@@ -14,8 +14,65 @@
 @end
 
 @interface DNSSRVRecord : DNSRecord
-  @property(nonatomic) NSInteger priority;
-  @property(nonatomic) NSInteger weight;
-  @property(nonatomic) NSInteger port;
+  @property(nonatomic) NSUInteger priority;
+  @property(nonatomic) NSUInteger weight;
+  @property(nonatomic) NSUInteger port;
   @property(nonatomic,retain) NSString *target;
+@end
+
+@interface DNSARecord     : DNSRecord
+  @property(nonatomic,retain) NSString *address;
+@end
+
+@interface DNSLOCRecord   : DNSRecord
+  @property(nonatomic) NSUInteger version;
+  @property(nonatomic) NSUInteger size;
+  @property(nonatomic) NSUInteger horizontal_precision;
+  @property(nonatomic) NSUInteger vertical_precision;
+  @property(nonatomic) NSUInteger latitude;
+  @property(nonatomic) NSUInteger longitude;
+  @property(nonatomic) NSUInteger altitude;
+@end
+
+@interface DNSMXRecord    : DNSRecord
+  @property(nonatomic) NSUInteger preference;
+  @property(nonatomic,retain) NSString *name;
+@end
+
+@interface DNSSOARecord   : DNSRecord
+  @property(nonatomic,retain) NSString *mname;
+  @property(nonatomic,retain) NSString *rname;
+  @property(nonatomic) NSUInteger serial;
+  @property(nonatomic) NSUInteger refresh;
+  @property(nonatomic) NSUInteger retry;
+  @property(nonatomic) NSUInteger expire;
+  @property(nonatomic) NSUInteger minimum;
+@end
+
+@interface DNSTXTRecord   : DNSRecord
+  @property(nonatomic,retain) NSArray *strings;
+@end
+
+@interface DNSnameRecord  : DNSRecord
+  @property(nonatomic,retain) NSString *name;
+@end
+@interface DNSCNAMERecord : DNSnameRecord
+@end
+@interface DNSMBRecord    : DNSnameRecord
+@end
+@interface DNSMGRecord    : DNSnameRecord
+@end
+@interface DNSMRRecord    : DNSnameRecord
+@end
+@interface DNSNSRecord    : DNSnameRecord
+@end
+@interface DNSPTRRecord   : DNSnameRecord
+@end
+
+@interface DNSAAAARecord  : DNSRecord
+  @property(nonatomic,retain) NSString *address;
+@end
+
+@interface DNSNULLRecord  : DNSRecord
+  @property(nonatomic,retain) NSData *octets;
 @end
